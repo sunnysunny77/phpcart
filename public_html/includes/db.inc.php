95c87b6b9ad43582f36f945b4c7132c9596aa354
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-11-10 07:23:21 +0800

    up

--- public_html/includes/db.inc.php
+++ public_html/includes/db.inc.php
@@ -1,10 +1,9 @@
 <?php
 
-include_once $_SERVER["DOCUMENT_ROOT"] . "/../keys.php"; 
 
 try
 {
-  $pdo = new PDO("mysql:host=localhost;dbname=cart", "root", $db);
+  $pdo = new PDO("mysql:host=localhost;dbname=cart", "root","");
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->exec("SET NAMES 'utf8'");
 }
